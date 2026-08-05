f4d50758b09da523f278e5ae98e78568322f234c
melinebgd <308437913+melinebgd@users.noreply.github.com>
2026-08-05 17:26:26 +0200

    Rename association page to About

--- src/data/site.ts
+++ src/data/site.ts
@@ -38,7 +38,7 @@ export const nav = [
     label: "Association",
     href: "/association",
     children: [
-      { label: "Missions & valeurs", href: "/association" },
+      { label: "À propos", href: "/association" },
       { label: "L’équipe", href: "/association/equipe" },
       { label: "#RaceAndCare", href: "/race-and-care" },
       { label: "Mécènes & ambassadeurs", href: "/mecenes" },
